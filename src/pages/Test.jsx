2eaf3d248f0e93bbd699ef1f21e1e84ed20b69fe
hlee123456 <hleesays@gmail.com>
2024-08-18 20:44:56 -0700

    Style: Vertically center the text in the section title textarea  and fill in the blank textarea

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -43,7 +43,6 @@ const Test = ({arrayOfProblems, numOfCorrect, setNumOfCorrect}) => {
                 <div 
                     ref={testFormatRef}
                     className="test__format">
-
                     {(isFillInTheBlank !== null) && 
                         (isFillInTheBlank) ? (
                             <FillInTheBlank
